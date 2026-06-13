3fec8df623ea68a080231f45563a0a91217dfb5c
Akk <7.aungkaungkhant@@gmail.com>
2026-06-13 11:33:26 +0630

    feat: implement persistent checkout system with orders relational tables

--- Database/init.sql
+++ Database/init.sql
@@ -9,3 +9,17 @@ INSERT INTO products (name, price) VALUES
 ('Coffee', 3.50),
 ('Fried Chicken', 7.25)
 ON CONFLICT DO NOTHING;
+
+CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT REFERENCES products(id),
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
